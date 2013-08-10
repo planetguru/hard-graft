5f94004b29fb8baae402e42476cf748a9be72cae
Christopher Lacy-Hulbert <clacy@yahoo-inc.com>
2013-08-10 11:51:29 +0100

    Added perl file and updated readme

---
+++ hard-graft.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+untie $me; open $doors;
+for($ever){ join
+$me, 'and', $you;
+last or sleep and die;}
+
+map 'our', $future or kill my $hopes;
+my %hope = ('to','be','with','you');
+open $doors unless $I&  break;
+$Read= each %hope and wait;
+
+times; shift and tell;
+warn my $friends; warn
+for($they_are_not_safe);
+$I& sort and study time;
+
+wait for($me);
+close $doors and sin
+bind (You_and, me);
+$please= wait and $see;
+
+if ($you){ chop( my $words);}
+my %hopes& break and die ;
+if($you){split 'from', $me;}
+$I= 'cry' and die 'inside';
+
+                                                                                                                                                                                                                                                         
+# a perl poem by christo
+# chris lacy-hulbert
+# October 12th 2003
